aae03699d309d6242021cd586498f6ba483cf48f
Robin Diddams <robindiddams@gmail.com>
2024-07-14 15:45:54 -0500

    add apikey

--- api/temp.js
+++ api/temp.js
@@ -1,8 +1,24 @@
 import { kv } from '@vercel/kv';
  
 export default async function handler(request, response) {
-  const temp = await kv.get('temp');
-  // return response.status(200).json({ temp });
-//  return temp as html
-  return response.status(200).send(temp);
+  // if method is Get then return the value of temp
+  if (request.method === 'GET') {
+    const temp = await kv.get('temp');
+    return response.status(200).send(temp + '°C');
+  }
+
+  // if method is Post then set the value of temp
+  if (request.method === 'POST') {
+    // check for api key from secret
+    if (request.headers['x-api-key'] !== process.env.API_KEY) {
+      return response.status(401).send('Unauthorized');
+    }
+
+    const body = JSON.parse(request.body);
+    await kv.put('temp', body.temp);
+    return response.status(200).send('OK');
+  }
+
+  // if method is not Get or Post then return 404
+  return response.status(404).send('Not Found');
 }
