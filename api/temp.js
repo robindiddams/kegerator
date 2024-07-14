b5a647e63a015738733ccda224c5ebcb89f4bdec
Robin Diddams <robindiddams@gmail.com>
2024-07-14 15:52:18 -0500

    dumb javascript

--- api/temp.js
+++ api/temp.js
@@ -14,7 +14,7 @@ export default async function handler(request, response) {
       return response.status(401).send('Unauthorized');
     }
 
-    await kv.put('temp', request.body.temp);
+    await kv.set('temp', request.body.temp);
     return response.status(200).send('OK');
   }
 
