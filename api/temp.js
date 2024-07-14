89b027e46cad79412d2c7dfc3655126f25b35eed
Robin Diddams <robindiddams@gmail.com>
2024-07-14 15:39:04 -0500

    more api work

--- api/temp.js
+++ api/temp.js
@@ -2,5 +2,7 @@ import { kv } from '@vercel/kv';
  
 export default async function handler(request, response) {
   const temp = await kv.get('temp');
-  return response.status(200).json({ temp });
+  // return response.status(200).json({ temp });
+//  return temp as html
+  return response.status(200).send(temp);
 }
